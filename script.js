87f09e8041d89d9e9ede93c5af82d8774001489a
I-Maged <magedhanafy@gmail.com>
2022-09-24 20:03:29 +0200

    first calculation

--- script.js
+++ script.js
@@ -1,38 +1,84 @@
 const numberBtns = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
 const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
 const userInput = document.querySelector('.user-input');
 const result = document.querySelector('.result');
 
 let firstNumber = '';
 let secondNumber = '';
-let operatorCkeck = false;
+let operatorCheck = false;
 let operation = '';
+
+equalBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', reset);
 
 numberBtns.forEach((number) => {
   number.addEventListener('click', (e) => {
     if (number.classList.contains('zero') && userInput.value == '') {
       return;
     }
-    userInput.value += e.target.innerText;
-    if (operatorCkeck || result.value != '') {
+
+    if (!operatorCheck && result.value != '' && userInput.value == '') {
+      userInput.value += e.target.innerText;
+      result.value = '';
+      firstNumber = e.target.innerText;
+    } else if (operatorCheck) {
+      userInput.value += e.target.innerText;
       secondNumber += e.target.innerText;
-      console.log(`secondNumber = ${secondNumber}`);
     } else {
+      userInput.value += e.target.innerText;
       firstNumber += e.target.innerText;
-      console.log(`firstNumber = ${firstNumber}`);
     }
   });
 });
 
 operatorBtns.forEach((operator) => {
   operator.addEventListener('click', (e) => {
-    operatorCkeck = true;
+    if (userInput.value == '' && firstNumber == '') {
+      return;
+    } else if (isNaN(userInput.value.slice(-1)) && firstNumber != '') {
+      userInput.value = userInput.value.slice(0, -1);
+    }
+    userInput.value += e.target.innerText;
+    operatorCheck = true;
+    operation = e.target.innerText;
   });
 });
 
-equalBtn.addEventListener('click', calculate);
-
 function calculate() {
-  userInput.value = Number(firstNumber) + Number(secondNumber);
+  let newResult = 0;
+  firstNumber = Number(firstNumber);
+  secondNumber = Number(secondNumber);
+
+  if (operation == '+') {
+    newResult = firstNumber + secondNumber;
+  } else if (operation == '-') {
+    newResult = firstNumber - secondNumber;
+  } else if (operation == '/') {
+    newResult = firstNumber / secondNumber;
+  } else if (operation == '*') {
+    newResult = firstNumber * secondNumber;
+  } else if (operation == '%') {
+    newResult = (firstNumber * 100) / secondNumber;
+  }
+
+  handleResult(newResult);
+}
+
+function handleResult(newResult) {
+  result.value = newResult;
+  firstNumber = newResult;
+  secondNumber = '';
+  userInput.value = '';
+  operatorCheck = false;
+}
+
+function reset() {
+  firstNumber = '';
+  secondNumber = '';
+  operatorCheck = false;
+  operation = '';
+  userInput.value = '';
+  result.value = '';
 }
